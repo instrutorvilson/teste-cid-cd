e64a73f873f3b3299f6a40255218648366049208
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-10 13:33:30 -0300

    Adicionado documentaçaõ com swagger

--- src/main/java/com/example/demo/controller/ContatoController.java
+++ src/main/java/com/example/demo/controller/ContatoController.java
@@ -1,17 +1,37 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.ContatoService;
 
 @RestController
 @RequestMapping("/contato")
 public class ContatoController {
-    
+    @Autowired
+    ContatoService service;
+	
 	@GetMapping
 	public ResponseEntity<String> ola(){
 		return ResponseEntity.status(HttpStatus.OK).body("Ola pessoal");
 	}
+	
+	@GetMapping("/somar/{n1}/{n2}")
+	public ResponseEntity<Integer> soma(@PathVariable("n1") int n1, @PathVariable("n2") int n2 ){
+		int resultado = 0;
+		resultado = service.somar(n1, n2);
+		return ResponseEntity.status(HttpStatus.OK).body(resultado);
+	}
+	
+	@GetMapping("/ola/{nome}")
+	public ResponseEntity<String> oi(@PathVariable("nome") String nome){
+		return ResponseEntity.status(HttpStatus.OK).body("Ola " + nome);
+	}
+	
+	
 }
---
+++ src/main/java/com/example/demo/service/ContatoService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContatoService {
+  
+	public int somar(int n1, int n2) {
+		int resultado = 0;
+		if(n1 > 5) {
+			n1 = n1;
+		}
+		else {
+			n1 = n1;
+		}
+		resultado = n1 + n2;
+		return resultado;
+	}
+}
